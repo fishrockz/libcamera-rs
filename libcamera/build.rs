a582c89f6999d52d444a9d24529e6c224360e8ee
William Salmon <pointswaves@gmail.com>
2024-12-20 16:50:07 +0000

    Update update version of libcamera selected

--- libcamera/build.rs
+++ libcamera/build.rs
@@ -6,7 +6,7 @@ use std::{
 use semver::{Comparator, Op, Version};
 
 fn main() {
-    let libcamera_version = Version::new(0, 3, 2);
+    let libcamera_version = Version::new(0, 4, 0);
 
     let versioned_files = Path::new("versioned_files");
     let mut candidates = std::fs::read_dir(versioned_files)
