4123d8ae8c34875aea397e8ca06805e3ad9ae9c4
William Salmon <pointswaves@gmail.com>
2025-01-19 20:46:58 +0000

    tweak control

--- libcamera/examples/list_cameras.rs
+++ libcamera/examples/list_cameras.rs
@@ -1,4 +1,11 @@
-use libcamera::{camera_manager::CameraManager, logging::LoggingLevel, stream::StreamRole};
+use libcamera::{
+    camera_manager::CameraManager,
+    control::ControlList,
+    controls::{Contrast, ControlId},
+    logging::LoggingLevel,
+    stream::StreamRole,
+};
+use libcamera_sys::libcamera_control_value;
 
 fn main() {
     let mgr = CameraManager::new().unwrap();
@@ -16,6 +23,28 @@ fn main() {
 
         let config = cam.generate_configuration(&[StreamRole::ViewFinder]).unwrap();
         let view_finder_cfg = config.get(0).unwrap();
-        println!("Available formats: {:#?}", view_finder_cfg.formats());
+        // println!("Available formats: {:#?}", view_finder_cfg.formats());
+
+        let controls = cam.controls();
+
+        let mut new_controls = ControlList::new();
+
+        for (num, info) in controls.into_iter() {
+            let id = ControlId::try_from(num).unwrap();
+            println!("{:?} {:?}", id, info.def());
+            //println!("{:?} {:?}", num, info.get());
+            println!("{:?} {:?}", num, info.values());
+
+            if id == ControlId::Contrast {
+                let mut contrast = Contrast::try_from(info.def()).unwrap();
+                contrast.0 = 0.5;
+                new_controls.set(contrast);
+            }
+        }
+        let cam = cameras.get(0).expect("No cameras found");
+        let mut cam = cam.acquire().expect("Unable to acquire camera");
+        let mut cfgs = cam.generate_configuration(&[StreamRole::ViewFinder]).unwrap();
+        cam.configure(&mut cfgs).expect("Unable to configure camera");
+        cam.start(Some(&new_controls));
     }
 }
